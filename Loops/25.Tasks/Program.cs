28484b6584c260b539e7cf73ea16c0abe0668fdb
Farid <hseynli@list.ru>
2023-04-10 15:41:23 +0400

    ..

--- Loops/25.Tasks/Program.cs
+++ Loops/25.Tasks/Program.cs
@@ -1,1 +1,8 @@
-﻿
+﻿//1. İstifadəçi hər-hansı bir n ədədini daxil edir. Həmin n sayda qədər iterasiya etmək.
+//2. İstifadəçi hər-hansı bir n ədədini daxil edir. Həmin n sayda qədər iterasiya etmək və 5-ə tam bölünən ədədləri ekranda əks elətdirmək.
+//3. İstifadəçi 3 natural ədəd daxil edir. Onların ortağını tapın.
+//4. İstifadəçi 5 natural ədəd daxil edir. Onların kubunu tapan proqram yazın.
+//5. for dövründən isftifadə edərək vurma cədvəlini yazın.
+//6. for dövrü ilə faktorialı hesablayan proqram yazın.
+//7. 0-1000 aralığında dövr yaradın. İterasiya olunan ədədin tək və ya cüt olduğunu müəyyən edən proqram yazın.
+//8. Kənar istifadəçinin bank hesabında 145.10 qəpik pul var. Yazılan proqramda istifadəçi üç dəfə ödəniş edir. Bank hesabında yekun məbləği əks elətdirən proqram yazın. Əgər yekun məbləğ mənfi ola bilməz. 
